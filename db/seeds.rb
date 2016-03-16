7855831a9fc8ddf5178fdfff3ba356794874b825
Meera Bhardwaj <meerabhardwaj@gmail.com>
2016-03-16 09:21:13 -0700

    completed half 33, start back at New Topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,29 @@
-# Create Posts
+
+15.times do
+  Topic.create!(
+    name:  RandomData.random_sentence,
+    description:   RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
 50.times do
-# #1
   Post.create!(
-# #2
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
   )
 end
 posts = Post.all
 
-
-# Create Comments
-# #3
 100.times do
   Comment.create!(
-# #4
     post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
 
-puts "#{Post.count}"
-Post.find_or_create_by(title: "I am INDEMPOTENT!", body: "And I'm not out of control at all.")
-puts "#{Post.count}"
-
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
