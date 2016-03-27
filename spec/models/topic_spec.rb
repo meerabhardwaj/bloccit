0664581469325b8e105d79fa90b9d712f2b9b643
Meera Bhardwaj <meerabhardwaj@gmail.com>
2016-03-27 15:58:18 -0700

    completed 35 homework

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Topic, type: :model do
   let(:topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
 
   it { is_expected.to have_many(:posts) }
-  
+  it { is_expected.to validate_length_of(:name).is_at_least(15) }
+
   describe "attributes" do
     it "responds to name" do
       expect(topic).to respond_to(:name)
