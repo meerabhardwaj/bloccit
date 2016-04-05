7797f90e48b84c23dafebe9d2d3b7c3529579094
Meera Bhardwaj <meerabhardwaj@gmail.com>
2016-04-05 00:06:27 -0700

    completed checkpoint 29, there is broken code under topics/show.html.erb that I'm having trouble solving

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+5.times do
+   User.create!(
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
 
 15.times do
   Topic.create!(
@@ -23,7 +31,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+ user.update_attributes!(
+   email: 'youremail.com', # replace this with your personal email
+   password: 'helloworld'
+ )
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
