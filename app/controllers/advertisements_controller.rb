66d253a6a9b8426991339ba89b7c44d481ccc20a
Meera Bhardwaj <meerabhardwaj@gmail.com>
2016-05-02 17:49:49 -0700

    fixed last error, removed create and replaced with new for @advertisement

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -1,25 +1,25 @@
 class AdvertisementsController < ApplicationController
   def index
-    @advertisements = Advertisement.all
+    @advertisement = Advertisement.all
   end
 
   def show
-    @advertisements = Advertisement.find(params[:id])
+    @advertisement = Advertisement.find(params[:id])
   end
 
   def new
-    @advertisements = Advertisement.new
+    @advertisement = Advertisement.new
   end
 
   def create
-    @advertisements = Advertisement.create
-    @advertisements.title = params[:advertisement][:title]
-    @advertisements.copy = params[:advertisement][:copy]
-    @advertisements.price = params[:advertisement][:price]
+    @advertisement = Advertisement.new
+    @advertisement.title = params[:advertisement][:title]
+    @advertisement.copy = params[:advertisement][:copy]
+    @advertisement.price = params[:advertisement][:price]
 
-    if @advertisements.save
+    if @advertisement.save
       flash[:notice] = "Advertisement was saved."
-      redirect_to @advertisements
+      redirect_to @advertisement
     else
       flash[:error] = "there was an error saving the advertisement. Please try again."
       render :new
